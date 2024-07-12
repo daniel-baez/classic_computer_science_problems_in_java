195993e2ff8b4dd73e389181e22ec76a7009b3fc
Daniel Baez <daplay@gmail.com>
2024-07-12 08:35:49 -0500

    WIP

--- src/main/java/chapter3/WordSearchConstraint.java
+++ src/main/java/chapter3/WordSearchConstraint.java
@@ -31,47 +31,47 @@ import chapter3.WordGrid.GridLocation;
 
 public class WordSearchConstraint extends Constraint<String, List<GridLocation>> {
 
-	public WordSearchConstraint(List<String> words) {
-		super(words);
-	}
+    public WordSearchConstraint(List<String> words) {
+        super(words);
+    }
 
-	@Override
-	public boolean satisfied(Map<String, List<GridLocation>> assignment) {
-		// combine all GridLocations into one giant List
-		List<GridLocation> allLocations = assignment.values().stream()
-				.flatMap(Collection::stream).collect(Collectors.toList());
-		// a set will eliminate duplicates using equals()
-		Set<GridLocation> allLocationsSet = new HashSet<>(allLocations);
-		// if there are any duplicate grid locations then there is an overlap
-		return allLocations.size() == allLocationsSet.size();
-	}
+    @Override
+    public boolean satisfied(Map<String, List<GridLocation>> assignment) {
+        // combine all GridLocations into one giant List
+        List<GridLocation> allLocations = assignment.values().stream()
+            .flatMap(Collection::stream).collect(Collectors.toList());
+        // a set will eliminate duplicates using equals()
+        Set<GridLocation> allLocationsSet = new HashSet<>(allLocations);
+        // if there are any duplicate grid locations then there is an overlap
+        return allLocations.size() == allLocationsSet.size();
+    }
 
-	public static void main(String[] args) {
-		WordGrid grid = new WordGrid(9, 9);
-		List<String> words = List.of("DANIEL", "JOE", "ALEJANDRO", "SARAH", "SALLY");
-		// generate domains for all words
-		Map<String, List<List<GridLocation>>> domains = new HashMap<>();
-		for (String word : words) {
-			domains.put(word, grid.generateDomain(word));
-		}
-		CSP<String, List<GridLocation>> csp = new CSP<>(words, domains);
-		csp.addConstraint(new WordSearchConstraint(words));
-		Map<String, List<GridLocation>> solution = csp.backtrackingSearch();
-		if (solution == null) {
-			System.out.println("No solution found!");
-		} else {
-			Random random = new Random();
-			for (Entry<String, List<GridLocation>> item : solution.entrySet()) {
-				String word = item.getKey();
-				List<GridLocation> locations = item.getValue();
-				// random reverse half the time
-				if (random.nextBoolean()) {
-					Collections.reverse(locations);
-				}
-				grid.mark(word, locations);
-			}
-			System.out.println(grid);
-		}
-	}
+    public static void main(String[] args) {
+        WordGrid grid = new WordGrid(9, 9);
+        List<String> words = List.of("DANIEL", "JOE", "ALEJANDRO", "SARAH", "SALLY");
+        // generate domains for all words
+        Map<String, List<List<GridLocation>>> domains = new HashMap<>();
+        for (String word : words) {
+            domains.put(word, grid.generateDomain(word));
+        }
+        CSP<String, List<GridLocation>> csp = new CSP<>(words, domains);
+        csp.addConstraint(new WordSearchConstraint(words));
+        Map<String, List<GridLocation>> solution = csp.backtrackingSearch();
+        if (solution == null) {
+            System.out.println("No solution found!");
+        } else {
+            Random random = new Random();
+            for (Entry<String, List<GridLocation>> item : solution.entrySet()) {
+                String word = item.getKey();
+                List<GridLocation> locations = item.getValue();
+                // random reverse half the time
+                if (random.nextBoolean()) {
+                    Collections.reverse(locations);
+                }
+                grid.mark(word, locations);
+            }
+            System.out.println(grid);
+        }
+    }
 
 }
